447b02da7f7bbbc1a950e94f4293d2a49e2b0f03
mightoverlord <noneofyourbeezywax>
2021-10-12 14:43:59 +0200

    first PoC

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # climatedata-assignment
 Assignment analyzing climate weather data
+
+## Problem Introduction
+Predicting max temperature for tomorrow using one year of past weather data, in Seattle. This is a supervised, regerssion machine learning problem. The features are data for the city, and the target is the temperature. 
+
+## Dataset 
+Data is available on the National centers for environmental information. Other datasets can be found in [this link](https://www.ncdc.noaa.gov/cdo-web/datasets)
+
+## Roadmap 
+This repository follows the following steps: 
+1. State the question and required data
+2. Acquire data 
+3. Identify and preprocess data: missing data, anomalies
+4. Prepare data for modelling
+5. Establish a baseline model 
+6. Train model on trainig data
+7. Make predictions on test data
+8. Compare predictions and calculate performance metrics 
+9. If performance not satisfactory: adjust parameters, acquire more data or use different modeling techniques 
+10. Interpret model and report results
+
+## Run package
+
+create a virtual environment 
+
+`sudo apt-get install graphviz`
+`mkdir env data/anomoly_detection data/hist data/heatmap data/boxplot`
+`python{version} -m venv env`
+`pip install -r requirements/base.txt`
--- src/main.py
+++ src/main.py
@@ -1,43 +1,56 @@
+from matplotlib.pyplot import draw_if_interactive
 from scipy.sparse import data
 from ingestion import Dataset
 from preperation import DataPrep
 from description import Descriptives
 from model import DataModel
+from visualization import DataVisualization
+import os
 
 '''
-Ingest, parse, descriptive analytics of Iris dataset 
-
-Hypothesis, when is a flower a setosa, versicolor or verginica 
-what are key predictories 
-
-K-means cluster is unsupervised, the variable is labeled so 
-random forest 
+rewrite the main
 '''
+
+dirname = os.getcwd()
 
 def start_ingestion():
-    DataClass = Dataset()
-    print(DataClass.describe_data())
-    DataClass.find_na()
+    data_class = Dataset()
+    print(data_class.describe_data())
+    data_class.find_na()
 
-    return DataClass.data
-
-def start_parsing(df):
-   DataPreperation =  DataPrep(df)
-   return DataPreperation.create_dummy_variable(), DataPreperation
+    return data_class.data
 
 def start_descriptive_analytics(df):
-    DataDescription = Descriptives(df)
-    print(DataDescription.assess_distribution())
+    data_descr = Descriptives(df)
+    for i in df.columns:
+        data_descr.assess_distribution(i)
 
-def start_model(data_prep): 
-    x_train, x_validation, y_train, y_validation = data_prep.create_train_test_set()
-    data_model = DataModel(x_train,x_validation, y_train, y_validation)
-    data_model.predication_models()
-    data_model.prediction_visualization()
-    data_model.make_predictions()
-    data_model.test_harnass()
+def start_parsing(df):
+   data_prep =  DataPrep(df)
+   data_prep.detecting_anomolies("scatterplot.jpg")
+   data_prep.remove_anomolies()
+   data_prep.detecting_anomolies("scatterplot_removed_anomolies.jpg")
+   return data_prep.create_train_test_set()
+   
+def start_model(x_train, x_val, y_train, y_val, feature_list): 
+    data_model = DataModel(x_train,x_val, y_train, y_val, feature_list)
+    data_model.establish_baseline()
+    data_model.make_predictions(data_model.train_model)
+    data_model.make_predictions(data_model.train_model_small)
+    data_model.make_predictions(data_model.train_model_important_feature)
+    return data_model
+
+def start_visualiation(data_model, df):
+    data_visualization = DataVisualization(data_model)
+    data_visualization.visualize_tree_graph()
+    data_visualization.visualize_tree_small()
+    data_visualization.variable_importance_visualiation()
+
+
     
 if __name__ == '__main__':
     df = start_ingestion()
-    df, data_prep = start_parsing(df)
-    start_model(data_prep)
+    start_descriptive_analytics(df)
+    x_train, x_val, y_train, y_val, feature_list = start_parsing(df)
+    data_model = start_model(x_train, x_val, y_train, y_val, feature_list)
+    start_visualiation(data_model)
